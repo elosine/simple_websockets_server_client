43e06ae17d33d9f9166c52c4ced814448a652d29
elosine <justinwenloyang@gmail.com>
2020-03-02 09:07:11 -0500

    uu

---
+++ server.js
@@ -0,0 +1,17 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(80);
+// WARNING: app.listen(80) will NOT work here!
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('framect', 99);
+});
+
+var hrTime = process.hrtime()
+console.log(hrTime[0]);
